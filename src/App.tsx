1fe24fcf2a3dd51f66aa4c7a3b34f5794d5b038c
meisammofidi <meisam.mofidi@yahoo.com>
2023-06-27 15:34:32 -0400

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-
-  return <Button colorScheme='blue'>Add Card</Button>
-  
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem pl={2} bg="tomato" area="nav">
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem pl={2} bg="gold" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl={2} bg="dodgerblue" area="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
