d771d48d47dcded3351510b77894bd04b093309d
meisammofidi <meisam.mofidi@yahoo.com>
2023-06-28 10:00:36 -0400

    Add GameCardContainer component

--- src/components/game-card/GameCard.tsx
+++ src/components/game-card/GameCard.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width={'300px'} borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card height='350px'>
+      <Image src={game.background_image} height='200px' />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={'space-between'}>
