fe0e68240e96ce0dd0fbf55303bfbad9aca7d3a8
meisammofidi <meisam.mofidi@yahoo.com>
2023-06-28 13:28:58 -0400

    Disply skeleton in fetching genres list

--- src/components/genres/GenreList.tsx
+++ src/components/genres/GenreList.tsx
@@ -1,16 +1,38 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonCircle,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react';
 import useGenres from '../../hooks/useGenres';
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading } = useGenres();
+  const genreSkeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  if (isLoading) {
+    return genreSkeletons.map((genre) => (
+      <Box key={genre} padding="6" boxShadow="lg" bg="dark">
+        <SkeletonCircle size="5" />
+        <SkeletonText mt="4" noOfLines={1} />
+      </Box>
+    ));
+  }
   return (
     <List>
       {genres &&
         genres.map((genre) => (
           <ListItem key={genre.id} paddingY={3}>
             <HStack>
-              <Image src={genre.image_background} boxSize='32px' borderRadius={8}/>
-              <Text fontSize='lg'>{genre.name}</Text>
+              <Image
+                src={genre.image_background}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
             </HStack>
           </ListItem>
         ))}
